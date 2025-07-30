6a5fd3d0e7225bf6a8fbdd2e8cd3a8ca328cca53
Wilbur <lwb980210@gmail.com>
2025-07-30 11:06:29 +1000

    数据库文件修改

--- deploy/dev/sql/init.sql
+++ deploy/dev/sql/init.sql
@@ -1,4 +1,5 @@
-表名小写  多个单词 下划线隔开  全部以tb开头
+-- 管理员用户
+-- 表名小写  多个单词 下划线隔开  全部以tb开头
 create table tb_sys_user (
 user_id      bigint unsigned not null comment '用户id（主键）',
 user_account varchar(20) not null  comment '账号',
@@ -12,32 +13,25 @@ primary key (`user_id`),
 unique key `idx_user_account` (`user_account`)
 );
 
-追踪数据创建、修改的历史寻找责任人
-
-
-
 --题库管理
+-- B端：列表功能、添加题目、编辑、删除
+-- C端：题库列表功能、题目热榜列表、答题、竞赛开始答题、竞赛练习
+-- 题目数据      ----》数据库  mysql    设计表结构
+-- 满足需求  避免冗余设计  考虑今后发展
+-- 题目内容： 长度变化
+-- 两数之和  1 + 2 = 3
+-- [
+--     {
+--         "input": "1 2",
+--         "output": "3"
+--     },
+--     {
+--         "input": "4 5",
+--         "output": "9"
+--     }
+-- ]
 
-B端：列表功能、添加题目、编辑、删除
-
-
-C端：题库列表功能、题目热榜列表、答题、竞赛开始答题、竞赛练习
-题目数据      ----》数据库  mysql    设计表结构
-满足需求  避免冗余设计  考虑今后发展
-题目内容： 长度变化
-两数之和  1 + 2 = 3
-
-[
-    {
-        "input": "1 2",
-        "output": "3"
-    },
-    {
-        "input": "4 5",
-        "output": "9"
-    }
-]
-时间限制 ：毫秒    空间限制：字节
+-- 时间限制 ：毫秒    空间限制：字节
 delete  from  tb_question where question_id =
 create table tb_question(
 question_id bigint unsigned not null comment '题目id',
@@ -57,16 +51,11 @@ primary key(`question_id`)
 );
 
 
-
-
-
-
 --竞赛管理
-B端：列表、新增、编辑、删除、发布、撤销发布
-
-C端：列表（未开始、历史） 、报名参赛、我的比赛、参加竞赛（竞赛倒计时、竞赛内题目切换、完成竞赛） 、竞赛练习、查看排名、我的消息
-是否开赛 不需要加在数据库中，我们实时计算就可以
-报名参赛：未开始、未报名
+-- B端：列表、新增、编辑、删除、发布、撤销发布
+-- C端：列表（未开始、历史）、报名参赛、我的比赛、参加竞赛（竞赛倒计时、竞赛内题目切换、完成竞赛）、竞赛练习、查看排名、我的消息
+-- 是否开赛 不需要加在数据库中，我们实时计算就可以
+-- 报名参赛：未开始、未报名
 create table tb_exam (
 exam_id  bigint unsigned not null comment '竞赛id（主键）',
 title varchar(50) not null comment '竞赛标题',
@@ -79,16 +68,16 @@ create_time  datetime not null comment '创建时间',
 update_by    bigint unsigned  comment '更新人',
 update_time  datetime comment '更新时间',
 primary key(exam_id)
-)
+);
 
-题目和竞赛之间是多对一的关系
-题目和竞赛的关系表
-题目id：  1    2   3  4
-竞赛id：999
-记录1： 主键id   1    999
-记录2： 主键id   2    999
-记录3： 主键id   3    999
-记录3： 主键id   4    999
+-- 题目和竞赛之间是多对一的关系
+-- 题目和竞赛的关系表
+-- 题目id：  1    2   3  4
+-- 竞赛id：999
+-- 记录1： 主键id   1    999
+-- 记录2： 主键id   2    999
+-- 记录3： 主键id   3    999
+-- 记录3： 主键id   4    999
 select * from  tb_exam_question   where exam_id = 1;
 delete  from tb_exam_question where exam_id = 1;
 create table tb_exam_question (
@@ -101,24 +90,10 @@ create_time  datetime not null comment '创建时间',
 update_by    bigint unsigned  comment '更新人',
 update_time  datetime comment '更新时间',
 primary key(exam_question_id)
-)
-
-
-
-
-
-
-
-
-
-
-
-
+);
 
 -- 用户管理（C端）
 -- B端：列表、修改用户状态
-
-
 -- C端：登录、注册、退出登录、个人中心
 
 create table tb_user(
@@ -140,11 +115,9 @@ create_time  datetime not null comment '创建时间',
 update_by    bigint unsigned  comment '更新人',
 update_time  datetime comment '更新时间',
 primary key(`user_id`)
-)
+);
 
-
-
-竞赛报名：
+-- 竞赛报名：
 create table tb_user_exam(
 user_exam_id bigint unsigned NOT NULL COMMENT '用户竞赛关系id',
 user_id bigint unsigned NOT NULL COMMENT '用户id',
@@ -156,7 +129,7 @@ create_time  datetime not null comment '创建时间',
 update_by    bigint unsigned  comment '更新人',
 update_time  datetime comment '更新时间',
 primary key(user_exam_id)
-)
+);
 
 select * from tb_user_exam where exam_id = 1  and  user_id = 100;
 
@@ -164,12 +137,7 @@ select user_id from tb_user_exam where exam_id = 1;
 
 select exam_id from tb_user_exam where user_id = 100;
 
-
-
-
-
-
-用户提交表
+-- 用户提交表
 create table tb_user_submit(
 submit_id bigint unsigned NOT NULL COMMENT '提交记录id',
 user_id   bigint unsigned NOT NULL COMMENT '用户id',
@@ -185,28 +153,26 @@ create_time  datetime not null comment '创建时间',
 update_by    bigint unsigned  comment '更新人',
 update_time  datetime comment '更新时间',
 primary key(`submit_id`)
-)
+);
 
-
-
-我的消息功能
-
-站内信：网站内部的一种通信方式。
- 1.用户和用户之间的通信。（点对点）
- 2.管理员/系统  和 某个用户之间的通信。（点对点）  ===》竞赛结果的通信消息
- 3. 管理员/系统 和 某个用户群（指的是满足某一条件的用户的群体）之间的通信。（点对面）
-
-1W
-
-数据库表的设计
-
-主键id  消息标题  消息内容  接收人    发送人
-1      福利信息   内容      1         0
-1      福利信息   内容      2         0
-1      福利信息   内容      3         0
-.........
-
-消息内容表
+-- 我的消息功能
+--
+-- 站内信：网站内部的一种通信方式。
+--  1.用户和用户之间的通信。（点对点）
+--  2.管理员/系统  和 某个用户之间的通信。（点对点）  ===》竞赛结果的通信消息
+--  3. 管理员/系统 和 某个用户群（指的是满足某一条件的用户的群体）之间的通信。（点对面）
+--
+-- 1W
+--
+-- 数据库表的设计
+--
+-- 主键id  消息标题  消息内容  接收人    发送人
+-- 1      福利信息   内容      1         0
+-- 1      福利信息   内容      2         0
+-- 1      福利信息   内容      3         0
+-- .........
+--
+-- 消息内容表
 create table tb_message_text(
 text_id  bigint unsigned NOT NULL COMMENT '消息内容id（主键）',
 message_title varchar(10)  NOT NULL COMMENT '消息标题',
@@ -216,11 +182,9 @@ create_time  datetime not null comment '创建时间',
 update_by    bigint unsigned  comment '更新人',
 update_time  datetime comment '更新时间',
 primary key (text_id)
-)
+);
 
-
-
-消息表
+-- 消息表
 create table tb_message(
 message_id  bigint unsigned NOT NULL COMMENT '消息id（主键）',
 text_id  bigint unsigned NOT NULL COMMENT '消息内容id（主键）',
@@ -231,7 +195,7 @@ create_time  datetime not null comment '创建时间',
 update_by    bigint unsigned  comment '更新人',
 update_time  datetime comment '更新时间',
 primary key (message_id)
-)
+);
 
 
 
